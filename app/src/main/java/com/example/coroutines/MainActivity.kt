6dae044db4e02069c4e75ac2e389364f6c6f177c
davidmoralesluis <davidmoralesluis@danielcastelao.org>
2023-06-19 00:33:54 +0200

    Generador de alcance y concurrencia

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.coroutines
 
-import android.graphics.Color
 import android.os.Bundle
-import android.util.Log
 import android.widget.Button
 import androidx.activity.ComponentActivity
 import com.example.constraintlayout.R
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.coroutineScope
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
@@ -20,18 +16,24 @@ class MainActivity : ComponentActivity() {
         val button = findViewById<Button>(R.id.button)
 
         button.setOnClickListener {
-            // primera rutina
-            primero()
+            runBlocking {
+                coroutinSuspend()
+                println("Final")
+            }
+
         }
     }
-    fun primero() = runBlocking {
-        launch { primeroSuspend() }
-        println("Hello") // main coroutine continues while a previous one is delayed
-    }
 
-    suspend fun primeroSuspend() = coroutineScope { // this: CoroutineScope
+    suspend fun coroutinSuspend() = coroutineScope { // this: CoroutineScope
+        launch {
+            delay(2000L) // non-blocking delay for 2 second (default time unit is ms)
+            println("World_2sec") // print after delay
+        }
+        launch {
             delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
-            println("World!") // print after delay
+            println("World_1sec") // print after delay
+        }
+        println("Hello")
         }
 
 }
