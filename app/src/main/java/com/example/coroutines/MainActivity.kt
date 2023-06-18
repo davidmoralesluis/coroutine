2c64459435f0ffa356979b0f0111c08f5557ad07
davidmoralesluis <davidmoralesluis@danielcastelao.org>
2023-06-18 23:22:10 +0200

    primera rutina

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -1,13 +1,35 @@
 package com.example.coroutines
 
+import android.graphics.Color
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
 import androidx.activity.ComponentActivity
 import com.example.constraintlayout.R
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+
+        button.setOnClickListener {
+            // primera rutina
+            primero()
+        }
+    }
+
+    fun primero() = runBlocking { // this: CoroutineScope
+        launch { // launch a new coroutine and continue
+            delay(1000L) // non-blocking delay for 1 second (default time unit is ms)
+            println("World!") // print after delay
+        }
+        println("Hello") // main coroutine continues while a previous one is delayed
     }
 
 }
